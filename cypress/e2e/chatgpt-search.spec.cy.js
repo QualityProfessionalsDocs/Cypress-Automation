490a4df174a676d959abab5790c0d7ec131a172e
QualityProfessionalsDocs <162305534+QualityProfessionalsDocs@users.noreply.github.com>
2024-03-10 13:21:25 +0530

    Update chatgpt-search.spec.cy.js

--- cypress/e2e/chatgpt-search.spec.cy.js
+++ cypress/e2e/chatgpt-search.spec.cy.js
@@ -1,9 +1,1 @@
-describe("Google search", () => {
-  it('searches for "what is cypress" by pressing enter', () => {
-    cy.visit("https://www.google.com/");
-    cy.get('[name="q"]').type("what is cypress{enter}");
-    cy.contains("Cypress.io: JavaScript End to End Testing Framework").should(
-      "be.visible"
-    );
-  });
-});
+
